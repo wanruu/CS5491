46a0c7b8782be157935564e17b5666942dd43c58
wanruuu <1155107790@link.cuhk.edu.hk>
2022-11-16 17:05:59 +0800

    remove para def in model to main

--- model.py
+++ model.py
@@ -18,27 +18,22 @@ def fc_relu_layer(in_chann, out_chann):
 
 
 class VGG16(nn.Module):
-    def __init__(self, class_num, conv_paras, pool_paras):
+    def __init__(self, class_num, conv_paras, pool_paras, fc_paras):
         """
         conv_paras: a length-13 list of tuple for convolution layer. Each tuple is (in_chann, out_chann, kernerl_size, padding)
-        pool_paras: a length-5 list of tuple for pooling layer. Each tuple is (kernel_size, stride)
+        pool_paras: a length-5 list of tuple for max-pooling layer. Each tuple is (kernel_size, stride)
+        fc_paras: a length-2 list of tuple for fully connection layer. Each tuple is (in_chann, out_chann) 
         """
         super().__init__()
         
         # Convolution + ReLU layer
         conv_layers = [conv_relu_layer(a, b, c, d) for a, b, c, d in conv_paras]
-        # pooling layer
-        pool_layers = [nn.MaxPool2d(kernel_size=k_size, stride=stride) for k_size, stride in pool_paras]
-
-
+        # Pooling + ReLU layer
+        pool_layers = [nn.MaxPool2d(kernel_size=a, stride=b) for a, b in pool_paras]
         # Full connected layers
-        fc_layers = nn.Sequential(
-            fc_relu_layer(7*7*512, 4096),
-            fc_relu_layer(4096, 4096)
-        )
-
+        fc_layers = [fc_relu_layer(a, b) for a, b in fc_paras]
         # Final layer
-        final_layer = nn.Linear(4096, class_num)
+        final_layer = nn.Linear(fc_paras[-1][1], class_num)
 
         # Connect layers
         self.layers = nn.Sequential(
@@ -47,7 +42,7 @@ class VGG16(nn.Module):
             conv_layers[4], conv_layers[5], conv_layers[6], pool_layers[2],
             conv_layers[7], conv_layers[8], conv_layers[9], pool_layers[3],
             conv_layers[10], conv_layers[11], conv_layers[12], pool_layers[4],
-            fc_layers,
+            fc_layers[0], fc_layers[1],
             final_layer
         )
 
