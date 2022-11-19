5a218e4607d2b57d95432e0944dfa7cdd2814f47
wr <1155107790@link.cuhk.edu.hk>
2022-11-20 00:08:13 +0800

    fix bug

--- VGG16/model/dfl_vgg16.py
+++ VGG16/model/dfl_vgg16.py
@@ -1,14 +1,14 @@
 import torch.nn as nn
 
 class DFL_VGG16(nn.Module):
-    def __init__(self, class_num, k, vgg, conv6_weight=[], conv6_bias=[]):
+    def __init__(self, class_num, k, vgg, conv6_weight=None, conv6_bias=None):
         """
         params class_num: M
         params k        : filters/class
         """
         super().__init__()
         self.name = "DFL_VGG16"
-        if conv6_bias or conv6_weight:
+        if conv6_bias is not None or conv6_weight is not None:
             self.name += "_Unrandom"
         self.class_num = class_num
         self.k = k
@@ -29,9 +29,9 @@ class DFL_VGG16(nn.Module):
         # P-Stream: mid
         conv4_chann_out = vgg.conv_paras[9][1]
         self.conv6 = conv6 = nn.Conv2d(conv4_chann_out, k*class_num, kernel_size=1, stride=1)  # filters grouped into {class_num}, out = kM*H*W
-        if conv6_weight:
+        if conv6_weight is not None:
             self.conv6.weight.data = conv6_weight
-        if conv6_bias:
+        if conv6_bias is not None:
             self.conv6.bias.data = conv6_bias
         self.pool6 = nn.MaxPool2d(kernel_size=12, stride=12)  # out = kM*1*1
         self.p_fc_layers = nn.Sequential(
